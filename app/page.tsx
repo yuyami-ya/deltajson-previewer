bde11a3769bcf820648ec23932e78785d1987ea6
MiyazawaYuya <miyazawa_y@kokopelli-inc.com>
2024-12-22 07:55:31 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,270 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import breaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm';
+import 'github-markdown-css/github-markdown.css';
+import '../styles/markdownStyles.css';
+
+interface Mention {
+    id: string;
+    value: string;
+}
+
+interface Insert {
+    mention?: Mention;
+    styled_mention?: Mention;
+    text?: string;
+    link?: string;
+}
+
+interface Attribute {
+    list?: string;
+    indent?: number;
+    link?: string;
+    blockquote?: boolean;
+    bold?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+    strike?: boolean;
+    color?: string;
+    background?: string;
+    code_block?: boolean;
+    header?: number;
+    image?: string;
+}
+
+interface OpsItem {
+    insert: Insert | string;
+    attributes?: Attribute;
+}
+
+interface Delta {
+    ops: OpsItem[];
+}
+
+function convertToMarkdown(delta: Delta): string {
+    const markdownLines: string[] = [];
+
+    delta.ops.forEach((op: OpsItem) => {
+        try {
+            if (typeof op.insert === 'object') {
+                if (op.insert.mention) {
+                    markdownLines.push(`@${op.insert.mention.value}`);
+                } else {
+                    markdownLines.push(JSON.stringify(op));
+                }
+                return;
+            }
+
+            const text = op.insert.trim();
+            const attributes = op.attributes || {};
+
+            if (attributes.header) {
+                const level = attributes.header;
+                markdownLines.push(`${'#'.repeat(level)} ${text}`);
+            } else if (attributes.bold) {
+                markdownLines.push(`**${text}**`);
+            } else if (attributes.italic) {
+                markdownLines.push(`*${text}*`);
+            } else if (attributes.underline) {
+                markdownLines.push(`<u>${text}</u>`);
+            } else if (attributes.link) {
+                markdownLines.push(`[${text}](${attributes.link})`);
+            } else if (attributes['code-block']) {
+                markdownLines.push(`\`\`\`\n${text}\n\`\`\``);
+            } else if (attributes.blockquote) {
+                markdownLines.push(`> ${text}`);
+            } else if (attributes.image) {
+                markdownLines.push(`![Image](${attributes.image})`);
+            } else if (attributes.list) {
+                const listType = attributes.list;
+                const indent = attributes.indent || 0;
+                const spaces = ' '.repeat(indent * 4);
+                if (listType === 'bullet') {
+                    markdownLines[-1] = `${spaces}- ${markdownLines[-1]}`;
+                } else if (listType === 'ordered') {
+                    markdownLines[-1] = `${spaces}1. ${markdownLines[-1]}`;
+                }
+            } else {
+                markdownLines.push(text);
+            }
+            console.log(markdownLines);
+        } catch (e) {
+            console.error(e);
+            markdownLines.push(op);
+        }
+    });
+
+    return markdownLines.join('\n');
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    // const [body, setBody] = useState<Delta>({
+    //     ops: [
+    //         { insert: { mention: { id: "123", value: "田中美和" } } },
+    //         { insert: "First open this link, then open this link." },
+    //         { insert: "\n" },
+    //         { insert: "https://sample.com/1", attributes: { link: "https://sample.com/1" } },
+    //         { insert: "\n" },
+    //         { insert: "https://sample.com/2", attributes: { link: "https://sample.com/2" } },
+    //         { insert: "\n" },
+    //         { insert: "Finally, please open the following link:" },
+    //         { insert: "\n" },
+    //         { insert: "https://sample.com/3", attributes: { link: "https://sample.com/3" } },
+    //         { insert: "\n" }
+    //     ]
+    // });
+    const [body, setBody] = useState<Delta>({
+            "ops": [
+                {
+                "insert": "This is a sample"
+                },
+                {
+                "insert": "\n"
+                },
+                {
+                "insert": "Parent List 1"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet"
+                }
+                },
+                {
+                "insert": "Parent List 2 When you insert a link in a list, a link is added to the child element."
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet"
+                }
+                },
+                {
+                "insert": "https://sample.com/1",
+                "attributes": {
+                    "link": "https://sample.com/1"
+                }
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet",
+                    "indent": 1
+                }
+                },
+                {
+                "insert": "Parent List 3"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet"
+                }
+                },
+                {
+                "insert": "Child List 1"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet",
+                    "indent": 1
+                }
+                },
+                {
+                "insert": "Child List 2 Underline style will be reset"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet",
+                    "indent": 1
+                }
+                },
+                {
+                "insert": "Child List 3"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "bullet",
+                    "indent": 1
+                }
+                },
+                {
+                "insert": ""
+                },
+                {
+                "insert": "\n"
+                },
+                {
+                "insert": "First Number List"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "ordered"
+                }
+                },
+                {
+                "insert": "Second Number List"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "ordered"
+                }
+                },
+                {
+                "insert": "Number list child elements"
+                },
+                {
+                "insert": "\n",
+                "attributes": {
+                    "list": "ordered",
+                    "indent": 1
+                }
+                },
+                {
+                "insert": "Bold will also be reset"
+                },
+                {
+                "insert": "\n"
+                }
+            ]
+    });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    let markdown = "empty"
+    try {
+        markdown = convertToMarkdown(body);
+    } catch (e) {
+        console.error(e);
+        markdown = `error: ${e}`
+    }
+    // markdown = `
+    // # タイトル
+    // ## 見出し
+    // 本文の内容です。**太字**や*イタリック*も使えます。
+    // `;
+
+    return (
+        <div style={{ display: 'flex', height: '100vh', flexDirection: 'column' }}>
+            <div style={{ display: 'flex', flex: 1 }}>
+                <textarea
+                    style={{ width: '50%', padding: '10px' }}
+                    value={JSON.stringify(body, null, 2)}
+                    onChange={(e) => setBody(JSON.parse(e.target.value))}
+                />
+                <div className="markdown-body" style={{ width: '50%', padding: '10px', borderLeft: '1px solid #ccc' }}>
+                    <ReactMarkdown remarkPlugins={[remarkGfm, breaks]}>{markdown}</ReactMarkdown>
+                </div>
+            </div>
+            <div style={{ padding: '10px', borderTop: '1px solid #ccc' }}>
+                <pre>{markdown}</pre>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
